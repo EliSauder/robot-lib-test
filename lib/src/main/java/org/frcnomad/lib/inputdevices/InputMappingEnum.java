643b3dfe3205a1e3572083f5a40b7a57cfbc7f62
EliSauder <24995216+EliSauder@users.noreply.github.com>
2021-01-25 19:39:06 -0800

    feat: Startted adding input mapping

---
+++ lib/src/main/java/org/frcnomad/lib/inputdevices/InputMappingEnum.java
@@ -0,0 +1,9 @@
+package org.frcnomad.lib.inputdevices;
+
+public enum InputMappingEnum {
+    UNCATEGORIZED,
+    TRIGGER_DRIVE,
+    OG_TRIGGER_DRIVE,
+    DEFAULT_DRIVE,
+    BASE_MAP
+}
---
+++ lib/src/main/java/org/frcnomad/lib/inputdevices/NomadButton.java
@@ -0,0 +1,6 @@
+package org.frcnomad.lib.inputdevices;
+
+import edu.wpi.first.wpilibj2.command.button.Button;
+
+public class NomadButton extends Button {    
+}
